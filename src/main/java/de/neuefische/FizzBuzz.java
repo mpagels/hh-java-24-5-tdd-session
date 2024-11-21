6555035958a4eb6b6c1d8903c47ff115a8465472
Martin Pagels <office@martinpagels.de>
2024-11-21 10:16:28 +0100

    chore: refactor calulate method

--- src/main/java/de/neuefische/FizzBuzz.java
+++ src/main/java/de/neuefische/FizzBuzz.java
@@ -4,11 +4,9 @@ public class FizzBuzz {
     public static String calculate(int number) {
         if (number % 15 == 0) {
             return "FizzBuzz";
-        }
-        if (number % 3 == 0) {
+        } else if (number % 3 == 0) {
             return "Fizz";
-        }
-        if (number % 5 == 0) {
+        } else if (number % 5 == 0) {
             return "Buzz";
         }
         return String.valueOf(number);
